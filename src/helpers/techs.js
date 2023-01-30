8f387ae43addaec79af45c6fc357255ac5e715ef
Tomara Petty <tomararuth@gmail.com>
2023-01-29 19:06:10 -0800

    update about me verbiage

--- src/helpers/techs.js
+++ src/helpers/techs.js
@@ -1,46 +1,42 @@
 export const techs = [
-    {
-        name: 'React',
-        iconName: 'react',
-    },
-    {
-        name: 'Javascript',
-        iconName: 'javascript',
-    },
-    {
-        name: 'Python',
-        iconName: 'python',
-    },
-    {
-        name: 'Django',
-        iconName: 'django',
-    },
-    {
-        name: 'AWS',
-        iconName: 'mongodb',
-    },
-    {
-        name: 'SQL lite',
-        iconName: 'sqllite',
-    },
-    {
-        name: 'Mongo DB',
-        iconName: 'mongodb',
-    },
-    {
-        name: 'HTML 5',
-        iconName: 'html5',
-    },
-    {
-        name: 'CSS3',
-        iconName: 'css3',
-    },
-    {
-        name: 'Bootstrap',
-        iconName: 'bootstrap',
-    },
-    {
-        name: 'Github',
-        iconName: 'github',
-    }
-]
+  {
+    name: 'React',
+    iconName: 'react',
+  },
+  {
+    name: 'Javascript',
+    iconName: 'javascript',
+  },
+  {
+    name: 'Python',
+    iconName: 'python',
+  },
+  {
+    name: 'Django',
+    iconName: 'django',
+  },
+  {
+    name: 'AWS',
+    iconName: 'mongodb',
+  },
+  {
+    name: 'SQL lite',
+    iconName: 'sqllite',
+  },
+  {
+    name: 'Mongo DB',
+    iconName: 'mongodb',
+  },
+  {
+    name: 'Github',
+    iconName: 'github',
+  },
+  {
+    name: 'HTML 5',
+    iconName: 'html5',
+  },
+  {
+    name: 'CSS3',
+    iconName: 'css3',
+  },
+];
--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -10,7 +10,7 @@ function AboutMe() {
       </div>
       <div className='about__text'>
         <h1>ABOUT ME</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum corrupti fugiat totam iste aperiam, eum, odit perferendis quia et facilis aspernatur animi dolore unde molestiae sint soluta iure possimus accusantium?</p>
+        <p>As a full stack nerd and React enthusiast, I am constantly seeking new opportunities to expand my skills and knowledge. As a proud queer woman, I am also dedicated to advocating for diversity and inclusion in the tech industry. In my free time, you can find me making music or training Muay Thai.</p>
       </div>
     </div>
   )
