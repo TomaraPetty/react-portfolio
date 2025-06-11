a3195af7ed9b02f8f80254b22b0bb4537c1f0bb3
Tomara Petty <tomararuth@gmail.com>
2025-06-10 17:45:37 -0700

    update particles to latest syntax

--- src/screens/particles/Particles.jsx
+++ src/screens/particles/Particles.jsx
@@ -1,5 +1,5 @@
 import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
+import { loadSlim } from 'tsparticles-slim';
 import { Fragment, useCallback } from 'react';
 import './particles.css';
 
@@ -9,7 +9,7 @@ function ParticlesBG() {
     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
+    await loadSlim(engine);
   }, []);
 
   const particlesLoaded = useCallback(async container => {
@@ -27,7 +27,7 @@ function ParticlesBG() {
             // fullScreen: { enable: false },
             background: {
               color: {
-                value: 'F8F1F1',
+                value: "#F8F1F1",
               },
             },
             fpsLimit: 60,
@@ -61,23 +61,35 @@ function ParticlesBG() {
             },
             particles: {
               color: {
-                value: '',
+                value: "#5EAAA8",
               },
               links: {
-                // color: '#ffff5f',
                 color: "#5EAAA8",
                 distance: 150,
                 enable: true,
                 opacity: 0.8,
-                // width: 1,
+                width: 1,
               },
               move: {
                 direction: 'none',
                 enable: true,
-                outMode: 'bounce',
+                outMode: 'out',
                 random: false,
                 speed: 1,
                 straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              size: {
+                value: { min: 1, max: 3 },
               },
             },
           }}
