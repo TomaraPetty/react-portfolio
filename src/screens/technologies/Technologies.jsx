1f4a2a2fdf5353f0db6d6a1f888525b5bde9afcf
Tomara Petty <tomararuth@gmail.com>
2025-06-11 18:24:02 -0700

    update technologies style and verbiage

--- src/screens/technologies/Technologies.jsx
+++ src/screens/technologies/Technologies.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import DevIcon from "devicon-react-svg";
-import { techs } from '../../helpers/techs';
-import './technologies.css'
+import { techs } from "../../helpers/techs";
+import "./technologies.css";
 
 function Technologies() {
-    return (
-        <div className="technologies__container" id="technologies">
-            <div>
-                <h1 style={{textAlign: 'center'}}>Some technologies I use:</h1>
+  return (
+    <div className="technologies__container" id="technologies">
+      <h1 className="header">TECHNOLOGIES</h1>
+
+      <div className="tech__cards__container">
+        {techs.map((e, idx) => {
+          return (
+            <div key={idx} className="tech__card">
+              <DevIcon className="tech__icons" icon={e.iconName} />
+              <span style={{ margin: "5px", fontSize: "1rem" }}>{e.name}</span>
             </div>
-            <div className="tech__cards__container">
-                {techs.map((e, idx) => {
-                    return (
-                        <div key={idx} className='tech__card'>
-                            <DevIcon className='tech__icons' icon={e.iconName}  />
-                            <span style={{ margin: '5px', fontSize: '1rem'}}>{e.name}</span>
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Technologies
+export default Technologies;
